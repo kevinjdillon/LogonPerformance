694a0dde2f29379fd9032984ca6304c81e0828ff
Kevin Dillon <kevin.dillon@microsoft.com>
2021-02-09 15:44:00 -1000

    update verbose script

---
+++ GetLogonEvents_verbose.ps1
@@ -0,0 +1,272 @@
+﻿# GetLogonEvents_verbose
+#
+# Queries Group policy and Winlogon event logs for logon-related activities
+# Queries StartupXML files for Application startup activity during the user logon process
+#
+# Calculate individual Policy Download duration times
+# Detect Changes (bool) for CSE Processing
+# Estimate bandwidth for logon session
+# DC and User Discovery durations
+# DC Name being used for GPO & Script processing
+
+
+# Create array for all events that this script will return for export data at end
+$EventList = @()
+
+#Find Logon Processing GPO Events
+$logonActivities = Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-GroupPolicy/Operational';ID='4001'} -ErrorAction SilentlyContinue
+
+# Loop through each logon Correlation ID to find/filter events for each logon
+$logonActivities | ForEach-Object {
+    $ActivityID = $_.ActivityID
+    $UserName = ""
+
+    #Loop through each GPO event
+    $XPath = "*[System[EventID=4001 or EventID=5257 or EventID=5016 or EventID=5017 or EventID=5314 or EventID=7016 or EventID=8001] and System[Correlation[@ActivityID='$ActivityID']]]"
+    $GPOEvents = Get-WinEvent -FilterXPath $Xpath -LogName 'Microsoft-Windows-GroupPolicy/Operational' -ErrorAction SilentlyContinue
+    $GPOEvents | ForEach-Object {
+
+        $TimeCreated = ""
+        $MachineName = ""
+        $EventID = ""
+        $Action = ""
+        $CSEName = ""
+        $Duration = ""
+        $Bandwidth = ""
+        $ChangesDetected = ""
+        $Event4016 = ""
+        $Event4016XML = ""
+        $Operation = ""
+
+        
+        #Convert to XML
+        $GPOEvent = [xml]$_.ToXml()
+
+        #Get Variables
+        $TimeCreated = $_.TimeCreated
+        $MachineName = $_.MachineName
+        $EventID = $_.Id
+
+        switch($eventID){
+            4001 {$Action = "GPO Start"
+                  $UserName = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "PrincipalSamName"}).InnerText
+                  $StartPolicy = $TimeCreated
+                  $CSEName = "GPO Start"
+                  $WinlogonSearchStart = $StartPolicy.AddMinutes(-1)
+                  $WinlogonSearchStop = $StartPolicy.AddSeconds(2)
+                  }
+            5314 {$Action = "Bandwidth"
+                  $CSEName = "Bandwidth"
+                  $Duration = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "BandwidthInkbps"}).InnerText
+                  }
+            5257 {$Action = "Download Policies"
+                  $CSEName = "All Policies"
+                  $Duration = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "PolicyDownloadTimeElapsedInMilliseconds"}).InnerText
+                  }
+            5017 {$CSEName = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "Parameter"}).InnerText
+                  $Duration = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "OperationElaspedTimeInMilliSeconds"}).InnerText
+                  $Operation = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "OperationDescription"}).InnerText
+                  If ($Operation -eq "%%4132") {$Action = "Download Policies"}
+                  If ($Operation -eq "%%4120") {$Action = "Discover DC"}
+                  If ($Operation -eq "%%4118") {$Action = "Discover User"}
+                  } 
+            {$_ -in 5016,7016} {$Action = "CSE Processing"
+                  $CSEName = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "CSEExtensionName"}).InnerText
+                  $Duration = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "CSEElaspedTimeInMilliSeconds"}).InnerText
+                  # Query corresponding event 4016 to see if policy changes detected
+                  $XPath = "*[System[EventID=4016] and System[Correlation[@ActivityID='$ActivityID']] and EventData[(Data[@Name='CSEExtensionName']='$CSEName')]]"
+                  $Event4016 = Get-WinEvent -FilterXPath $Xpath -LogName 'Microsoft-Windows-GroupPolicy/Operational' -ErrorAction SilentlyContinue
+                  $Event4016XML = [xml]$Event4016.ToXml()
+                  $ChangesDetected = ($Event4016XML.Event.EventData.Data | Where-Object {$_.name -eq "IsGPOListChanged"}).InnerText
+                  }
+            8001 {$Action = "GPO Complete"
+                  $CSEName = "GPO Complete"
+                  $UserName = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "PrincipalSamName"}).InnerText
+                  $DurationInSec = ($GPOEvent.Event.EventData.Data | Where-Object {$_.name -eq "PolicyElaspedTimeInSeconds"}).InnerText
+                  $Duration = [int]$DurationInSec * 1000
+                  $StopPolicy = $TimeCreated.AddMinutes(1)
+                  $EndScriptSearch = $TimeCreated.AddMinutes(10)
+                  }
+            } #End Switch
+
+        $obj = New-Object -TypeName PSCustomObject -Property @{
+            SessionID = $ActivityID.Guid
+            TimeCreated = $TimeCreated.ToString("MM/dd/yyyy HH:mm:ss.fff")
+            MachineName = $MachineName
+            EventID = $EventID
+            Action = $Action
+            ActivityName = $CSEName
+            UserName = $UserName
+            Duration = $Duration
+            ChangesDetected = $ChangesDetected
+            }
+
+        $EventList += $obj
+
+        } # End Loop through each logon event
+
+
+    #Find Logon Scripts During time preiod
+    $scriptEvents = Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-GroupPolicy/Operational';ID='4018','5018';StartTime=$StartPolicy;EndTime=$EndScriptSearch} -ErrorAction SilentlyContinue
+    $scriptEvents | ForEach-Object {
+        $ScriptEvent = [xml]$_.ToXml()
+
+        $TimeCreated = ""
+        $MachineName = ""
+        $EventID = ""
+        $Action = ""
+        $CSEName = ""
+        $Duration = ""
+
+        #Get Variables
+        $TimeCreated = $_.TimeCreated
+        $MachineName = $_.MachineName
+        $EventID = $_.Id
+    
+        If (($ScriptEvent.Event.EventData.Data | Where-Object {$_.name -eq "ScriptType"}).InnerText -eq 1) {
+            
+            $UserName = ($ScriptEvent.Event.EventData.Data | Where-Object {$_.name -eq "PrincipalSamName"}).InnerText
+
+            switch($EventID){
+                4018 {$Action = "Logon Script Start"}
+                5018 {$Action = "Logon Script Complete"
+                      $DurationInSec = ($ScriptEvent.Event.EventData.Data | Where-Object {$_.name -eq "ScriptElaspedTimeInSeconds"}).InnerText
+                      $Duration = [int]$DurationInSec * 1000}
+            } # End Switch
+
+            $obj = New-Object -TypeName PSCustomObject -Property @{
+            SessionID = $ActivityID.Guid
+            TimeCreated = $TimeCreated.ToString("MM/dd/yyyy HH:mm:ss.fff")
+            MachineName = $MachineName
+            EventID = $EventID
+            Action = $Action
+            ActivityName = $Action
+            UserName = $UserName
+            Duration = $Duration
+            }
+
+            $EventList += $obj
+
+            } # End If
+        } # End Find Logon Scripts Loop
+
+    #Find Winlogon events During time preiod
+    $AuthActivities = Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-Winlogon/Operational';ID='1','2';StartTime=$WinlogonSearchStart;EndTime=$WinlogonSearchStop} | Select-Object -ErrorAction SilentlyContinue
+        $AuthActivities | ForEach-Object {
+
+            $TimeCreated = ""
+            $MachineName = ""
+            $EventID = ""
+            $Action = ""
+            $CSEName = ""
+            $Duration = ""
+
+            #Get Variables
+            $TimeCreated = $_.TimeCreated
+            $MachineName = $_.MachineName
+            $EventID = $_.Id
+
+            switch($EventID){
+                1 {$Action = "Authentication Start"}
+                2 {$Action = "Authentication Complete"}
+            } # End Switch
+
+            #$Duration = Event2 - Event1
+
+            $obj = New-Object -TypeName PSCustomObject -Property @{
+            SessionID = $ActivityID.Guid
+            TimeCreated = $TimeCreated.ToString("MM/dd/yyyy HH:mm:ss.fff")
+            MachineName = $MachineName
+            EventID = $EventID
+            Action = $Action
+            ActivityName = $Action
+            UserName = $UserName
+            Duration = $Duration
+            }
+
+            $EventList += $obj
+        } # End Winlogon Auth Activities
+
+
+    #Find Desktop launch events during logon timeperiod
+    $DesktopAvail = Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-GroupPolicy/Operational';ID='6339';StartTime=$StartPolicy;EndTime=$StopPolicy} -ErrorAction SilentlyContinue
+            $DesktopAvail | ForEach-Object {
+
+            $TimeCreated = ""
+            $MachineName = ""
+            $EventID = ""
+            $Action = ""
+            $CSEName = ""
+            $Duration = ""
+
+            #Get Variables
+            $TimeCreated = $_.TimeCreated
+            $MachineName = $_.MachineName
+            $EventID = $_.Id
+            $Action = "Desktop Initialize"
+
+            $obj = New-Object -TypeName PSCustomObject -Property @{
+            SessionID = $ActivityID.Guid
+            TimeCreated = $TimeCreated.ToString("MM/dd/yyyy HH:mm:ss.fff")
+            MachineName = $MachineName
+            EventID = $EventID
+            Action = $Action
+            ActivityName = $Action
+            UserName = $UserName
+            Duration = $Duration
+            }
+
+            $EventList += $obj
+            } # Loop through Desktop Launch events
+
+    #Find Application launch events
+    #Convert to XML
+    $AppEvent = [xml]$_.ToXml()
+
+    #Get Variables
+    $TimeCreated = $_.TimeCreated
+    $MachineName = $_.MachineName
+    $EventID = $_.Id
+    $UserName = ($AppEvent.Event.EventData.Data | Where-Object {$_.name -eq "PrincipalSamName"}).InnerText
+    $GPOProcessStart = $TimeCreated
+    $StartPolicy = $TimeCreated
+    $EndScriptSearch = $TimeCreated.AddMinutes(10)
+
+    $StartupXMLFile = Get-ChildItem "C:\Windows\System32\WDI\LogFiles\StartupInfo" | Where-Object {($_.LastWriteTime -gt $StartPolicy) -and ($_.LastWriteTime -lt $EndScriptSearch) } -ErrorAction SilentlyContinue
+    
+    If  (!$StartupXMLFile.FullName) {} # StartupInfo XML file Not Found for logon session!
+    Else {
+
+        [xml]$StartupXMLContent = Get-Content -path $StartupXMLFile.FullName
+
+        #Loop through each Process
+        ForEach ($ProcessItem in $StartupXMLContent.StartupData.Process) {
+
+            
+            $ProcessTime = $ProcessItem.StartTime
+            $ProcessDateTime = [datetime]::ParseExact($ProcessTime, 'yyyy/MM/dd:HH:mm:ss.fffffff', [System.Globalization.CultureInfo]::InvariantCulture)
+            $ProcessLocalTime = $ProcessDateTime.ToLocalTime()
+            $ProcessName = $ProcessItem.CommandLine.'#cdata-section'
+            $ProcessNameShort = $ProcessName.split("\")[-1]
+
+            $obj = New-Object -TypeName PSCustomObject -Property @{
+            TimeCreated = $ProcessLocalTime.ToString("MM/dd/yyyy HH:mm:ss.fff")
+            UserName = $UserName
+            Action = "Application Launch"
+            EventID = "StartupInfo.xml"
+            MachineName = $MachineName
+            SessionID = $ActivityID.Guid
+            Duration = ""
+            ActivityName = $ProcessNameShort
+            #ProcessParent = $ProcessItem.ParentName
+            }
+            $EventList += $obj
+
+
+        } # End ForEach - Create Obj
+    } # End If/Else
+
+} # End Loop through each logon Correlation ID (ActivityID)
+
+#Final Output
+$EventList | Export-Csv -Path "C:\Temp\LogonTimelines.csv" -NoTypeInformation
---
+++ MECM_JSON_Format_Merge.ps1
@@ -0,0 +1,135 @@
+﻿# JSON_Format_Merge
+#
+# Formats raw JSON files from SCCM Run Scripts output of 'GetLogonEvents' script
+# Cleans up formatting errors and removes truncated JSON outputs
+# Creates additional columns for BaseName, Delta, & Bandwidth
+# Performs calculations and cleanup on data
+# Creates csv file for each JSON file (per base)
+# Merges all csvs into single 'all_bases.csv'
+#
+# v3
+# Removed Bandwidth event row from logon sessions
+
+# Start JSON Formatting & CLeanup #
+Get-ChildItem "C:\Temp\JSONOutput" -Filter *.txt | 
+Foreach-Object {
+
+    $SessionIDs = @()
+    $JsonFile = ""
+    $JsonArray = ""
+    $JSONElement = ""
+    $JSONNew = ""
+    $NewJSONFile = "[`n"
+    $FormattedJSON = ""
+
+    $JsonFile = get-content -Raw $_.FullName
+    $JSONArray = $JSONFile -split("\[")
+    $BaseName = $_.BaseName
+
+    ForEach ($JSONElement in $JSONArray) {
+        If ($JSONElement.Length -gt 1) {
+
+            #Cleanup string                
+            $JSONElement = $JSONElement.Trim()
+            $JSONElement = $JSONElement.replace("`n","")
+            $JSONElement = $JSONElement.replace("`r`n","")
+
+            #get last character in string
+            $LastChar = $JSONElement.Substring($JSONElement.get_Length()-1)
+            #$LastChar
+
+            #Remove truncated output
+            If ($LastChar -ne "]") {
+            }
+            #Output Ok - add to new JSON variable
+            Else {
+                $JSONElement =$JSONElement.Substring(0,$JSONElement.Length-2)
+                $JSONNew = $JSONElement + ",`n"
+                $NewJSONFile += $JSONNew
+            }
+        }
+    }
+
+    $NewJSONFile = $NewJSONFile.Substring(0,$NewJSONFile.Length-2)
+    $NewJSONFile = $NewJSONFile.Replace("{{","{")
+    $NewJSONFile += "`r`n]"
+    $FormattedJSON = $NewJSONFile | Out-String | ConvertFrom-Json
+
+    # End JSON Formatting & CLeanup #
+
+    # Get Unique SessionID Values
+    ForEach ($Node in $FormattedJSON) {
+        $SessionIDs += $Node.SessionID
+        $Node | Add-Member -MemberType NoteProperty -Name 'Delta' -Value ""
+        $Node | Add-Member -MemberType NoteProperty -Name 'BaseName' -Value $BaseName
+        $Node | Add-Member -MemberType NoteProperty -Name 'Bandwidth' -Value ""
+    }
+    $UniqueSessions = $SessionIDs | Sort-Object | Get-Unique
+
+    #Loop through each Unique SessionID Value
+    ForEach ($SessionID in $UniqueSessions) {
+        $AuthStartTime = ""
+        $Bandwidth = ""
+
+        # Get Auth Start Time from Winlogon Event TimeCreated
+        $AuthStartTime = ($FormattedJSON | where {$_.SessionID -eq $SessionID -and $_.ActivityName -eq "Authentication Start"}).TimeCreated
+        $Bandwidth = ($FormattedJSON | where {$_.SessionID -eq $SessionID -and $_.EventID -eq 5314}).Duration
+
+        #Add Bandwidth speed value
+        If ($Bandwidth) {
+            $Node | Add-Member -MemberType NoteProperty -Name 'Bandwidth' -Value $Bandwidth -Force
+        }
+
+        # If Auth Start Time is Found for this session
+        If ($AuthStartTime) {
+
+            # Loop through each Node in this Unique Session and add Delta Value
+            ForEach ($Node in ($FormattedJSON | where {$_.SessionID -eq $SessionID})) {
+                $DeltaMS = ""
+                If ($AuthStartTime -is [array]) {
+                }
+                Else {
+                    $Delta = [datetime]$Node.TimeCreated - [datetime]$AuthStartTime
+                    $DeltaMS = [int]$Delta.TotalMilliseconds
+                    #$DeltaMS
+
+                    $Node | Add-Member -MemberType NoteProperty -Name 'Delta' -Value $DeltaMS -Force
+
+                    If ($Node.ActivityName -eq "Authentication Complete") {
+                        $Node | Add-Member -MemberType NoteProperty -Name 'Duration' -Value $DeltaMS -Force
+                    }
+                }
+            } #End For Node Loop
+        } # End If
+
+    } # End For SessionID Loop
+
+    # Remove Negative Delta values
+    $OutputJSON = $FormattedJSON | Where {$_.Delta -ge 0 -and $_.EventID -ne 5314}
+
+    $FileOut = "$PSScriptRoot\" + $_.BaseName + ".csv"
+    $OutputJSON | select * | export-csv ($FileOut) -NoTypeInformation
+    Write-Output "$FileOut successfully created" 
+
+} #End For - Loop to next file
+
+#Merge all csv files into one
+$CSVFiles = Get-ChildItem $PSScriptRoot -Filter *.csv
+$OutputFile = "$PSScriptRoot\AllBases.csv" 
+$Output = @()
+
+foreach($CSV in $CSVFiles) { 
+    $CSVFullPath = $CSV.FullName
+    if(Test-Path $CSVFullPath) { 
+         
+        $temp = Import-CSV -Path $CSVFullPath | select *
+        $Output += $temp 
+ 
+    } else { 
+        Write-Warning "$CSV : No such file found" 
+    } 
+ 
+} 
+$Output | Export-Csv -Path $OutputFile -NoTypeInformation 
+Write-Output "$OutputFile successfully created" 
+ 
